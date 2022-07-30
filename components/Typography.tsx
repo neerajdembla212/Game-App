1574d95927b6cd0d4ac74f00d8e53b5e138aeee3
Neeraj Dembla <dembla.neeraj212@gmail.com>
2022-07-30 15:25:22 +0800

    adding home page

--- components/Typography.tsx
+++ components/Typography.tsx
@@ -1,37 +1,65 @@
-import React, { FC } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, { FC } from "react";
+import { Text, StyleSheet } from "react-native";
 
 interface TypographyProps {
-    style: 'heading' | 'subHeading' | 'normal',
-    text: string
+  type: "heading" | "subHeading" | "normal";
+  text: string;
+  overrideStyle?: Object;
+  bold?: boolean;
+  color?: string;
 }
 
-export const Typography: FC<TypographyProps> = ({ style, text }) => {
-    function getStyle() {
-        switch (style) {
-            case 'heading': return styles.heading;
-            case 'subHeading': return styles.subHeading;
-            case 'normal': return styles.normal;
-            default: return styles.normal;
-        }
+export const Typography: FC<TypographyProps> = ({
+  type,
+  text,
+  overrideStyle = {},
+  bold = false,
+  color = "#FFFFFF",
+}) => {
+  function getStyle() {
+    const additionalStyles = {
+      ...overrideStyle,
+      fontWeight: bold ? "bold" : "normal",
+      color,
+    };
+    switch (type) {
+      case "heading":
+        return {
+          ...styles.heading,
+          ...additionalStyles,
+        };
+      case "subHeading":
+        return {
+          ...styles.subHeading,
+          ...additionalStyles,
+        };
+      case "normal":
+        return {
+          ...styles.normal,
+          ...additionalStyles,
+        };
+      default:
+        return {
+          ...styles.normal,
+          ...additionalStyles,
+        };
     }
-    return <Text style={getStyle()}>{text}</Text>
-}
+  }
+  return <Text style={getStyle()}>{text}</Text>;
+};
 
 const styles = StyleSheet.create({
-    heading: {
-        fontSize: 48,
-        color: '#FFFFFF',
-        fontWeight: 'bold',
-        lineHeight: 50,
-        marginBottom: 10
-    },
-    normal: {
-        color: '#FFFFFF',
-        fontSize: 16,
-        lineHeight: 20
-    },
-    subHeading: {
-
-    }
-})
+  heading: {
+    fontSize: 48,
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    lineHeight: 50,
+    marginBottom: 10,
+  },
+  normal: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    lineHeight: 20,
+  },
+  subHeading: {},
+});
